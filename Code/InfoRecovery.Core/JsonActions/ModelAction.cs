fd3cdae8f6c6d97de09a9f31cb0316886dfa2273
René Hernández <renehr9102@gmail.com>
2014-12-07 20:18:00 -0800

    Added json actions

---
+++ Code/InfoRecovery.Core/JsonActions/ModelAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InfoRecovery.Core.JsonActions
+{
+    public class ModelAction : IJsonSerializable
+    {
+        public string Action { get; set; }
+
+        public string Path { get; set; }
+
+        public string Query { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ Code/InfoRecovery.Core/JsonActions/TextResultAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InfoRecovery.Core.JsonActions
+{
+    public class TextResultAction : IJsonSerializable
+    {
+        public string  Action { get; set; }
+
+        public string[] Terms { get; set; }
+
+    }
+}
