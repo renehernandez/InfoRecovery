8c9dce8abe448e7960b0004f96b450e6f7025efc
René Hernández <renehr9102@gmail.com>
2014-12-08 04:34:54 -0800

    Working in build operation

--- Code/InfoRecovery.VectorialModel/Program.cs
+++ Code/InfoRecovery.VectorialModel/Program.cs
@@ -2,6 +2,7 @@
 using InfoRecovery.Core.JsonActions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -13,12 +14,34 @@ namespace InfoRecovery.VectorialModel
         {
             if (args.Length > 0)
             {
-                var modelAction = JsonHelper.ReadJson<ModelAction>(args[0]);
+                string jsonPath = args[0];
                 string textPath = args[1];
                 string indexPath = args[2];
 
+                var modelAction = JsonHelper.ReadJson<ModelAction>(jsonPath);
+                
                 if (modelAction.Action == "build")
                 {
+                    var action = new TextAction();
+                    TextResultAction result;
+                    var docs = DocumentReader.Read(modelAction.Path).ToArray();
+                    var create = new VectorialCreateAction();
+
+                    var info = new ProcessStartInfo(textPath){
+                        Arguments = jsonPath,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
+
+                    foreach (var tuple in DocumentReader.Read(modelAction.Path))
+                    {
+                        action.Action = "process";
+                        action.Data = tuple.Item2;
+                        JsonHelper.WriteJson(action, jsonPath);
+                        var proc = Process.Start(info);
+                        proc.WaitForExit();
+                        result = JsonHelper.ReadJson<TextResultAction>(jsonPath);
+                    }
                 }
                 else if (modelAction.Query != null)
                 {
