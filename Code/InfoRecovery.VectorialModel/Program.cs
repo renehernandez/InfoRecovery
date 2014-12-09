c6854742fe9f0b5d45616c19c1875e757667a216
René Hernández <renehr9102@gmail.com>
2014-12-09 14:55:08 -0800

    Fixed issues with build json creation and added query skeleton handling

--- Code/InfoRecovery.VectorialModel/Program.cs
+++ Code/InfoRecovery.VectorialModel/Program.cs
@@ -19,34 +19,36 @@ namespace InfoRecovery.VectorialModel
                 string indexPath = args[2];
 
                 var modelAction = JsonHelper.ReadJson<ModelAction>(jsonPath);
+                TextAction action;
+                TextResultAction result;
+                ModelSendAction create;
+
+                var info = new ProcessStartInfo(textPath)
+                {
+                    Arguments = jsonPath,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
                 
                 if (modelAction.Action == "build")
-                {
-
-                    var action = new TextAction();
-                    TextResultAction result;
-                    var create = new VectorialCreateAction() { Action = "create" };
-                    var data = new VectorialDataItem();
-                    var doc = new DocumentItem();
-
+                {              
+                    ModelData data;
+                    DocumentData doc;
+                    create = new ModelSendAction() { Action = "create" };
+                    
                     var docs = DocumentReader.Read(modelAction.Path).ToArray();
                     int documentsNumber = docs.Length;
                     var termPerDoc = new Dictionary<string, Dictionary<string, int>>();
-
-                    var info = new ProcessStartInfo(textPath){
-                        Arguments = jsonPath,
-                        UseShellExecute = false,
-                        CreateNoWindow = true
-                    };
+                    var normDoc = new Dictionary<string, double>();
 
                     foreach (var tuple in docs)
                     {
-                        //action.Action = "process";
-                        //action.Data = tuple.Item2;
-                        //JsonHelper.WriteJson(action, jsonPath);
-                        //var proc = Process.Start(info);
-                        //proc.WaitForExit();
-                        //result = JsonHelper.ReadJson<TextResultAction>(jsonPath);
+                        action.Action = "process";
+                        action.Data = tuple.Item2;
+                        JsonHelper.WriteJson(action, jsonPath);
+                        var proc = Process.Start(info);
+                        proc.WaitForExit();
+                        result = JsonHelper.ReadJson<TextResultAction>(jsonPath);
                         result = new TextResultAction() { Terms = tuple.Item2.Split() };
 
                         foreach (var term in result.Terms)
@@ -61,19 +63,34 @@ namespace InfoRecovery.VectorialModel
                         }
                     }
 
-                    create.Data = new List<VectorialDataItem>();
                     foreach (var keyValue in termPerDoc)
                     {
-                        data = new VectorialDataItem();
+                        double idf = Math.Log(documentsNumber * 1.0 / keyValue.Value.Count);
+                        foreach (var item in keyValue.Value)
+                        {
+                            if (!normDoc.ContainsKey(item.Key))
+                                normDoc[item.Key] = 0;
+
+                            int max = keyValue.Value.Values.Max();
+                            double tf = 0.5 + 0.5 * item.Value / max;
+                            normDoc[item.Key] += (idf * tf) * (idf * tf);
+                        }
+                    }
+
+                    create.Data = new List<ModelData>();
+                    foreach (var keyValue in termPerDoc)
+                    {
+                        data = new ModelData();
                         data.Key = keyValue.Key;
-                        data.Value = new TermItem();
+                        data.Value = new ValueData();
                         data.Value.Idf = Math.Log(documentsNumber * 1.0 / keyValue.Value.Count);
-                        data.Value.Documents = new List<DocumentItem>();
+                        data.Value.Documents = new List<DocumentData>();
                         int max = keyValue.Value.Values.Max();
 
                         foreach (var item in keyValue.Value)
                         {
-                            doc = new DocumentItem() {Document = item.Key, Tf = 0.5 + 0.5 * item.Value / max};
+                            double norm = Math.Sqrt(normDoc[item.Key]);
+                            doc = new DocumentData() {Document = item.Key, Tf = (0.5 + 0.5 * item.Value / max) / norm};
                             data.Value.Documents.Add(doc);
                         }
                         create.Data.Add(data);
@@ -86,12 +103,18 @@ namespace InfoRecovery.VectorialModel
                         UseShellExecute = false,
                         CreateNoWindow = true
                     };
+
                     var process = Process.Start(info);
                     process.WaitForExit();
                 }
                 else if (modelAction.Query != null)
                 {
-
+                    action.Action = "process";
+                    action.Data = modelAction.Query;
+                    JsonHelper.WriteJson(action, jsonPath);
+                    var proc = Process.Start(info);
+                    proc.WaitForExit();
+                    result = JsonHelper.ReadJson<TextResultAction>(jsonPath);
                 }
             }
 
