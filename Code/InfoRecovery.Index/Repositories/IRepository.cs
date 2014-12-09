d40ba844ede208938253c0b2cbeb8c1ca034e80e
jcurbelo <curbelorobin@gmail.com>
2014-12-09 15:03:39 -0800

    Changing 'IRepository'

--- Code/InfoRecovery.Index/Repositories/IRepository.cs
+++ Code/InfoRecovery.Index/Repositories/IRepository.cs
@@ -9,10 +9,10 @@ namespace InfoRecovery.Index.Repositories
     {
         IEnumerable<T> All();
         T Create(T instance);
-        T Update(T instance, T newInstance);
         T FirstOrCreate(T instance);
-        bool Exists(Dictionary<string, string> instance);
-        IEnumerable<T> WhereEquals(Dictionary<string, string> fieldValues);
+        T Update(T instance);
+        bool Exists(string sqlCondition);
+        IEnumerable<T> Where(string field, string oper, string value);
         void Delete(T instance);
 
     }
