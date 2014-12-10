693f3f6c31649ad0793edb2bd4a40c079d95ad36
jcurbelo <curbelorobin@gmail.com>
2014-12-09 16:30:10 -0800

    Prove of concept.

--- Code/InfoRecovery.Index/Program.cs
+++ Code/InfoRecovery.Index/Program.cs
@@ -9,6 +9,37 @@ namespace InfoRecovery.Index
     {
         static void Main(string[] args)
         {
+            var data = new DataManager();
+            var value1 = @"{
+                                idf: 5.4,
+                                    {
+                                        tf: 5.1,
+                                        document: 'path\to\document.pdf'
+                                    },
+                                    {
+                                        tf: 5.1,
+                                        document: 'path\to\document.pdf'
+                                    }
+                           }";
+            var value2 = @"{
+                                    {
+                                        document: 'path\to\document.pdf'
+                                    },
+                                    {
+                                        document: 'path\to\document.pdf'
+                                    }
+                           }";
+
+            data.Delete("key1");
+            data.Add("key1", value1);
+            data.Add("key2", value2);
+            data.Add("document.pdf", "get");
+            Console.WriteLine(data.Get("key1"));
+            data.Delete("key1");
+            data.Add("key1", "pepe");
+            Console.WriteLine(data.Get("key1"));
+          
         }
+
     }
 }
